e6e3b4ee8b6addc6fda232c174ca70476ad8d5ab
yattaeshop-coder <yattaeshop@gmail.com>
2026-07-21 03:45:56 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,8 @@
-const C="japan-v3";const A=["./","./index.html","./css/style.css","./js/app.js","./icons/icon.svg","./manifest.webmanifest"];self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match("./index.html")))));
+const CACHE_NAME = "japan-v5-1";
+self.addEventListener("install", event => { self.skipWaiting(); });
+self.addEventListener("activate", event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener("fetch", event => {
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+});
